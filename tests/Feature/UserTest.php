d90092107ebe210ddba7d39dd6e6767be007a03f
Ranimhasni06@gmail.com <106739055+ranimhosni@users.noreply.github.com>
2026-05-08 18:54:48 +0200

    update usertest

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,13 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseMigrations;
     public function test_example(): void
     {
         $response = $this->get('/');
